142f77d322604e4436bd672a63e422dc68fa6f36
Arezlon <50326537+Arezlon@users.noreply.github.com>
2021-04-08 03:37:47 -0300

    Actualización base de datos

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -7,5 +7,9 @@ namespace SSpartanoInmobiliaria.Models
 {
     public class Pago
     {
+        public int Id { get; set; }
+        public int ContratoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public Contrato Contrato { get; set; }
     }
 }
